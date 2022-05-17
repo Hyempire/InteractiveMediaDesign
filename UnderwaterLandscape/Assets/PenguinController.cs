712e5e6433f5b9fa8c785c7329c2458bfb67dbae
suriming <soolim520@gmail.com>
2022-05-17 17:37:24 +0900

    <ADD>Oculus jump connected

--- UnderwaterLandscape/Assets/PenguinController.cs
+++ UnderwaterLandscape/Assets/PenguinController.cs
@@ -6,7 +6,16 @@ public class PenguinController : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f;
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 7f;
+
+    public Transform groundCheck;
+    public float groundDistance = 4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     float xRotation = 0f;
     public float turnSpeed = 100f;
@@ -22,24 +31,45 @@ public class PenguinController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        myMove();
+        //CharacterRotation();
+    }
+
+    void myMove()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
 
-        CharacterRotation();
-    }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
 
-    public void CharacterRotation()
-    {
-        turn.x += Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime;
-        turn.y = Input.GetAxis("Mouse Y") * turnSpeed * Time.deltaTime;
-
-        //xRotation -= turn.y;
-        //overload방/
-        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(0, turn.x, 0f);
+        controller.Move(velocity * Time.deltaTime);
     }
+    //public void CharacterRotation()
+    //{
+    //    turn.x += Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime;
+    //    turn.y = Input.GetAxis("Mouse Y") * turnSpeed * Time.deltaTime;
+
+    //    //xRotation -= turn.y;
+    //    //overload방/
+    //    //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    //    transform.localRotation = Quaternion.Euler(0, turn.x, 0f);
+    //}
 
 }
--- UnderwaterLandscape/Assets/firstPersonCamera.cs
+++ UnderwaterLandscape/Assets/firstPersonCamera.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class firstPersonCamera : MonoBehaviour
 {
-    //public Transform penguinBody;
+    public Transform penguinBody;
     float xRotation = 0f;
-    public float turnSpeed = 100f;
+    public float turnSpeed = 200f;
+    public float turnSpeed2 = 500f;
     public Vector2 turn;
     public Vector3 deltaMove;
 
     void MouseRotation()
     {
+        float mouseX = Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime;
         turn.x += Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime;
         turn.y = Input.GetAxis("Mouse Y") * turnSpeed * Time.deltaTime;
 
         xRotation -= turn.y;
         //overload방/
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, turn.x, 0f);
+        xRotation = Mathf.Clamp(xRotation, -45f, 80f);
 
-        //penguinBody.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0f);
+        //penguinBody.localRotation = Quaternion.Euler(0, turn.x, 0f);
+        penguinBody.Rotate(Vector3.up * mouseX);
 
     }
     // Start is called before the first frame update
